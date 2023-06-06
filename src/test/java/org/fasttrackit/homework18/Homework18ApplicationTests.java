b1f06183059d214d0f436e295dcca79084e21648
Alex Paltinean <alex.paltinean@appointfix.com>
2023-06-06 18:56:37 +0300

    first commit

---
+++ src/test/java/org/fasttrackit/homework18/Homework18ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.homework18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
